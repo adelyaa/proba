2476113f517e6d6e87897579e67caec540aee367
adelyaa <32518026+adelyaa@users.noreply.github.com>
2017-12-13 01:17:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine($"ВВЕДИТЕ КОЭФФИЦИЕНТЫ");
+            int a=int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Calculator.Diskr(a, b, c);
+        }
+    }
+    class Calculator
+    {
+        public static void Diskr(int x, int y, int z)
+        {
+            double d = y * y - 4 * x * z;
+            if (d < 0)
+                Console.WriteLine("ДИСКРИМИНАНТ МЕНЬШЕ НУЛЯ,НЕВЕРНЫЕ КОЭФФЦИЕНТЫ");
+            else
+            {
+                Console.WriteLine("х1 = " + (-y + Math.Sqrt(d)) / (2 * x));
+                Console.WriteLine("х2 = " + (-y - Math.Sqrt(d)) / (2 * x));
+            }
+        }
+       
+    }
+}
